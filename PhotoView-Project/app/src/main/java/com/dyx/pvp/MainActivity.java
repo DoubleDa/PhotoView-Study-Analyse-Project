039bcb34716307c60f5b2ea5394cf099a58f1d0d
DoubleDa <dayonxin@gmail.com>
2016-07-19 23:59:36 +0800

    Commit Simple Example

--- PhotoView-Project/app/src/main/java/com/dyx/pvp/MainActivity.java
+++ PhotoView-Project/app/src/main/java/com/dyx/pvp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.dyx.pvp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+public class MainActivity extends Activity {
+
+    @Bind(R.id.iv)
+    ImageView iv;
+    private PhotoViewAttacher attacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Drawable drawable = getResources().getDrawable(R.mipmap.my_img);
+        iv.setImageDrawable(drawable);
+
+        attacher = new PhotoViewAttacher(iv);
     }
+
+
 }
